8c70c58fa026aa20b7e5f0080846573076256ee1
almalee24 <flores.almalee@gmail.com>
2020-12-08 23:43:46 +0200

    added validates and attributes to contact model

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,4 +1,13 @@
 class Contact < ApplicationRecord
-    has_many :connections
     belongs_to :user
+    has_many :connections, dependent: :destroy 
+    accepts_nested_attributes_for :connections, reject_if: proc {|att| att['created_at'].blank? || att["take_away"].blank?}
+
+    validates :first_name, presence: true
+    validates :last_name, presence: true
+    validates :email, presence: true
+
+    def created_time
+        self.created_at.strftime("%b %e -- %l:%M %p")
+    end
 end
