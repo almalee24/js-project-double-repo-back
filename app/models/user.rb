b22ca8d1091f3b966b024419978deef5cf6c3dd1
almalee24 <flores.almalee@gmail.com>
2020-12-08 23:47:38 +0200

    added validations to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,7 @@ class User < ApplicationRecord
     has_many :positions
     has_many :contacts
     has_many :conections, through: :contacts
+
+    validates :name, presence: true
+    validates :email, presence: true 
 end
