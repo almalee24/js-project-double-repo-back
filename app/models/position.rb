79cd20fa8038c081ecccbd4560842cb4f9cadbcf
almalee24 <flores.almalee@gmail.com>
2020-12-08 23:46:44 +0200

    added validations to position model

--- app/models/position.rb
+++ app/models/position.rb
@@ -1,3 +1,8 @@
 class Position < ApplicationRecord
-    belongs_to :user
+    belongs_to
+
+    validates :company_name, presence: true
+    validates :location, presence: true
+    validates :description, presence: true
+    validates :title, presence: true
 end
