a4d68d0cd5a76094ac319f0430cb0182e12053c6
almalee24 <flores.almalee@gmail.com>
2020-12-08 23:19:51 +0200

    finished position controller

--- app/controllers/positions_controller.rb
+++ app/controllers/positions_controller.rb
@@ -1,16 +1,50 @@
 class PositionsController < ApplicationController
+    before_action :set_positon, only: [:show, :update, :destory]
 
+    # GET /positions
     def index
-        positions = Position.all
-        render json: positions: [:title, :company_name, :location]
+        @positions = Position.all
+        render json: @positions: [:title, :company_name, :location]
     end
 
+    # GET /positions/1
     def show
-        position = Position.find_by_id(id: params[:id])
-        render json: position
+        render json: @position
     end
 
-    def edit
-        
+    # POSTS /posts
+    def create
+        @position = Position.new(position_params)
+
+        if @position.save
+            render json: @position
+        else   
+            render json: {errors: @position.errors.full_messages.to_sentence}
+        end
     end
+
+    # PATCH/PUT /positions/1
+    def update
+        if @positon.update(position_params)
+            render json: @position_params
+        else   
+            render json: {errors: @position.errors.full_messages.to_sentence}
+        end
+    end
+
+    # DELETE /positions/1
+    def destory
+        @position.destory
+        render json: @position
+    end
+
+    private 
+
+        def set_positon
+            @position = Position.find(params[:id])
+        end
+
+        def position_params
+            params.require(:position).permit(:company_name, :title, :location, :description, :applied, :applied_date)
+        end
 end
